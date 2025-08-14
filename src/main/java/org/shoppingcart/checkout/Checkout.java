333dca1fe60b9895b7224c90c4e0160e32df65b7
Kundan Sharma <kundan.scorpio@gmail.com>
2025-08-14 11:19:08 +0100

    Refactored checkout class.

--- src/main/java/org/shoppingcart/checkout/Checkout.java
+++ src/main/java/org/shoppingcart/checkout/Checkout.java
@@ -1,33 +1,37 @@
 package org.shoppingcart.checkout;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Checkout {
     private static final double APPLE_PRICE = 0.60;
     private static final double ORANGE_PRICE = 0.25;
+
+    Predicate<String> isApple = item-> item.equalsIgnoreCase("Apple");
+    Predicate<String> isOrange = item-> item.equalsIgnoreCase("Orange");
 
     public double calculateTotal(List<String> items) {
         if (items == null || items.isEmpty()) {
             return 0.0;
         }
 
-        double total = 0.0;
-        for (String item : items) {
-            if ("Apple".equalsIgnoreCase(item)) {
-                total += APPLE_PRICE;
-            } else if ("Orange".equalsIgnoreCase(item)) {
-                total += ORANGE_PRICE;
-            }
-        }
-        return total;
+        return items.stream()
+                .mapToDouble(item -> {
+                    if ("Apple".equalsIgnoreCase(item)) {
+                        return APPLE_PRICE;
+                    } else if ("Orange".equalsIgnoreCase(item)) {
+                        return ORANGE_PRICE;
+                    }
+                    return 0.0;
+                })
+                .sum();
     }
-
-    public double calculateTotalWithOffers(List<String> items) {
+        public double calculateTotalWithOffers(List<String> items) {
         if (items == null || items.isEmpty()) {
             return 0.0;
         }
-        long appleCount = items.stream().filter(item -> item.equalsIgnoreCase("Apple")).count();
-        long orangeCount = items.stream().filter(item -> item.equalsIgnoreCase("Orange")).count();
+        long appleCount = items.stream().filter(isApple).count();
+        long orangeCount = items.stream().filter(isOrange).count();
 
         // Buy One Get One Free on Apples
         long applesToPayFor = (appleCount / 2) + (appleCount % 2);
